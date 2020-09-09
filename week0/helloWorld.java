fadb5d4cf4417553e2624bece8376e0bb5a2400b
Joseph Grigoletti <jgrig2@gmail.com>
2020-09-09 02:25:40 -0500

    finished prequesites and now starting basic code. hello world with java done

---
+++ week0/helloWorld.java
@@ -0,0 +1,21 @@
+//reference from memory and verififed through textbook Java how to program 10th ed. by Deitel & compiler. p. 40-41
+
+//printing a singling line to the console with 1 statement. 
+
+//start of global class titled helloWorld
+public class helloWorld {
+
+    //The main method starts here
+    public static void main(String[] args)
+
+    {
+        System.out.println("Hello World");
+
+    } 
+    //the main method ends here
+}
+//end of global class "helloWorld"
+
+//to compile & excute the program follow the 2 step processus. 
+//1.in the terminal type "javac" + "filename" when you are in the working directory (to find use pwd if needed)
+//2. in the terminal type "java" + "filename" to execute the app. 
