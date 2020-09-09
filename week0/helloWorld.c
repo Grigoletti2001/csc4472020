f96f2b2f78ccc432c074e5e24b2e95a9be514446
Joseph Grigoletti <jgrig2@gmail.com>
2020-09-09 02:45:09 -0500

    compiler instructions for java and c, c code started

---
+++ week0/helloWorld.c
@@ -0,0 +1,16 @@
+//first c program
+//hello World. 
+//must include in header of program. this is a pre-processor directive. They are functions. 
+#include<stdio.h>
+//main is the function (it has nothing in the parenthese so it will return nothing, thus it void.) main () is also called a "colelction os statements". these can be called with arguments, but note it is being called without any paramaters.
+//"In C, if a function signature doesn’t specify any argument, it means that the function can be called with any number of parameters or without any parameters."
+
+void main() {
+
+    printf("hello World");
+    //a function. prints the values we want to see. in this case a string. we must end the function with a semicolon
+    //the semicon means it is the termination of a statement (statement terminator in c language.)
+
+}
+
+//how to compile and execute the app ? https: //stackoverflow.com/questions/32337643/how-to-run-c-program-on-mac-os-x-using-terminal 
