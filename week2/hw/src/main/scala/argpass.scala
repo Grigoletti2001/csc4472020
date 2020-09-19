cbc065525670a9a38e6376e8f99eb5c3cde2ea0c
Joseph Grigoletti <jgrig2@gmail.com>
2020-09-19 12:18:12 -0500

    done

--- week2/hw/src/main/scala/argpass.scala
+++ week2/hw/src/main/scala/argpass.scala
@@ -37,7 +37,8 @@ object argpass {
  }
 
   // EXERCISE 1: complete the following higher-order function.
-  // It has one parameter f: a function that takes an instance of RefInt (see above for the definition of the class RefInt) and returns Unit (i.e., nothing interesting).
+  // It has one parameter f: a function that takes an instance of RefInt (see above for the definition 
+  of the class RefInt) and returns Unit (i.e., nothing interesting).
   // Your code must create ONLY ONE instance of RefInt, then call f three times.
   // f will update the integer stored in the instances of RefInt it is given.
   // Your code must return a tuple of the three integers provided by f in the order that they came back from calls, i.e., the integer from the first call to f is the first integer in the returned tuple.
@@ -49,6 +50,7 @@ object argpass {
     // val n : Int = r.get
     (-1, -1, -1)
   }
+  
 
   // EXERCISE 2: complete the following higher-order function.
   // It has one parameter f: a function that takes an instance of RefInt (see above for the definition of the class RefInt) and returns Unit (i.e., nothing interesting).
